22b28b2ee1367c1c751a8948c2be52d22213ce6a
danu <danujagreru@gmail.com>
2024-01-31 10:50:50 +0530

    create Temp.java

---
+++ src/generics/Temp.java
@@ -0,0 +1,12 @@
+package generics;
+
+/*
+    @author DanujaV
+    @created 1/31/24 - 10:50 AM   
+*/
+
+public class Temp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
